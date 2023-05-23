8ff723e936b4aa857b86481d1fcd36ab3741ec0c
OniRemon <saito4431@gmail.com>
2023-05-23 17:15:15 +0900

    commit

---
+++ app/src/main/java/com/example/appcompose/GHUsers.kt
@@ -0,0 +1,19 @@
+package com.example.appcompose
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class GHUsers(
+    @SerialName("id")
+    val id : Int,
+    @SerialName("login")
+    val login : String,
+    @SerialName("public_repos")
+    val publicRepos : Int,
+    @SerialName("repos_url")
+    val reposUrl : String,
+    @SerialName("updated_at")
+    val updatedAt : String,
+)
--- app/src/main/java/com/example/appcompose/MainActivity.kt
+++ app/src/main/java/com/example/appcompose/MainActivity.kt
@@ -10,6 +10,23 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import android.util.Log
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.appcompose.ui.theme.AppComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +39,133 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
+                }
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+}
+
+enum class Route {
+    HOME,
+    COMM,
+    REPOS,
+    INPUT,
+}
+const val TAG = "タグ"
+@Composable
+fun MainScreen(viewModel: MainViewModel = androidx.lifecycle.viewmodel.compose.viewModel()){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Route.HOME.name ) {
+        composable(Route.HOME.name) {
+            HomeScreen(viewModel,navController)
+        }
+        composable(Route.COMM.name) {
+            CommScreen(viewModel,navController)
+        }
+        composable(Route.INPUT.name) {
+            InputScreen(viewModel,navController)
+        }
+        composable(Route.REPOS.name) {
+            ReposScreen(viewModel)
+        }
+    }
+}
+
+@Composable
+fun HomeScreen(
+    viewModel: MainViewModel = androidx.lifecycle.viewmodel.compose.viewModel(),
+    navController: NavController = rememberNavController()
+) {
+    Column(modifier = Modifier
+        .padding(16.dp)
+        .fillMaxWidth()) {
+        val isAndroid = viewModel.isAndroid.collectAsState()
+
+        val dateText = viewModel.dateText.collectAsState()
+        if (isAndroid.value) {
+            Greeting(name = "Android")
+        } else {
+            Greeting(name = "iPhone")
+        }
+        Button(onClick = {
+            viewModel.onClick()
+        }) {
+            Text(text = "Push me!!")
+        }
+        Text(text = dateText.value)
+
+        Button(onClick = { navController.navigate(Route.COMM.name)
+        }) {
+            Text(text = "Navigate CommScreen")
+        }
+    }
+}
+
+@Composable
+fun CommScreen(
+    viewModel: MainViewModel = androidx.lifecycle.viewmodel.compose.viewModel(),
+    navController: NavController = rememberNavController()
+) {
+    Column(modifier = Modifier
+        .padding(16.dp)
+        .fillMaxWidth()) {
+        Greeting(name = "CommScreen")
+        val login = viewModel.login.collectAsState()
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable {
+                    viewModel.onUpdateDraftLogin(login.value)
+                    navController.navigate(Route.INPUT.name)
+                },
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = login.value)
+            Button(
+                onClick = {
+                    viewModel.onGet()
+                    navController.navigate(Route.REPOS.name)
+                }
+            ) {
+                Text(text = "onGet!!")
+            }
+        }
+        Divider()
+        val names = listOf("microsoft", "apple", "google")
+        LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)){
+            items(names) {
+                Row(modifier = Modifier
+                    .clickable { viewModel.onSelect(it) }
+                    .fillMaxWidth()) {
+                    Text(text = it)
                 }
             }
         }
@@ -30,14 +173,99 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun InputScreen(
+    viewModel: MainViewModel = androidx.lifecycle.viewmodel.compose.viewModel(),
+    navController: NavController = rememberNavController()
+) {
+    val draftLogin = viewModel.draftLogin.collectAsState()
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(16.dp)) {
+        TextField(
+            value = draftLogin.value,
+            onValueChange = {
+                if (it.length < 20) {
+                    viewModel.onUpdateDraftLogin(it)
+                }
+            },
+            label = { Text(text = "user") },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(onDone = {
+                viewModel.onSelect(draftLogin.value)
+                navController.popBackStack()
+            } ),
+            singleLine = true
+        )
+    }
+}
+
+@Composable
+fun ReposScreen(viewModel: MainViewModel = androidx.lifecycle.viewmodel.compose.viewModel()){
+    Column(modifier = Modifier
+        .fillMaxWidth()) {
+        val login = viewModel.login.collectAsState()
+        val repos = viewModel.repos.collectAsState()
+        Text(
+            text = "user = ${login.value} size = ${repos.value.size}",
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+        Divider()
+        LazyColumn{
+            items(repos.value) {
+                Column(modifier = Modifier.padding(vertical = 8.dp)) {
+                    Text(text = it.name, fontSize = 16.sp, modifier = Modifier.padding(horizontal = 16.dp))
+                    Text(text = it.updatedAt, fontSize = 8.sp, modifier = Modifier.padding(horizontal = 16.dp))
+                }
+            }
+        }
+    }
+    val inProgress = viewModel.inProgress.collectAsState()
+    if (inProgress.value) {
+        Box(modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center) {
+            CircularProgressIndicator()
+        }
+    }
+    val message = viewModel.message.collectAsState()
+    if (message.value.isNotEmpty()) {
+        AlertDialog(
+            onDismissRequest = { viewModel.onDismiss() },
+            buttons = {},
+            text = { Text(text = message.value) })
+    }
+}
+@Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun MainPreview() {
     AppComposeTheme {
-        Greeting("Android")
+        HomeScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CommPreview() {
+    AppComposeTheme {
+        CommScreen()
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun InputPreview() {
+    AppComposeTheme {
+        InputScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ReposPreview() {
+    AppComposeTheme {
+        ReposScreen()
     }
 }
